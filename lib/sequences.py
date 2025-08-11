4335354e2b22a97a6b98ba153947ee1dc63d724a
felona jepchumba <felonajepchumba@gmail.com>
2025-08-11 10:45:58 +0300

    data structures sequences

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    
+    if length <= 0:
+        print([])
+        return
+    elif length == 1:
+        print([0])
+        return
+    fib_list = [0, 1]
+    while len(fib_list) < length:
+        next_fib = fib_list[-1] + fib_list[-2]
+        fib_list.append(next_fib)
+        
+    print(fib_list)
